fe7ba91d2699a62585e8c7ca73c3bfe161cfbef1
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-11-09 04:05:38 -0200

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Work+Sans:400,600,700');
+
+html {
+  font-family: 'Work Sans', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  color: #333;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+a {
+  color: #333;
+  transition: opacity 0.2s ease, transform 0.2s ease;
+  display: inline-block;
+
+  &:hover {
+    opacity: 0.8;
+    transform: translateY(-2px) scale(1.02);
+  }
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        <img src="~/assets/logo_symbol.png"> <span class="text">Henrique Kuwai</span>
+      </h1>
+      <h2 class="subtitle">
+        JavaScript lover, developing since 2008.<br>
+        Focusing in front-end since 2015.
+        <template v-if="employee">
+          <br>Currently employed in
+          <a
+            :href="employeeUrl"
+            class="highlight">{{ employee }}</a>.
+        </template>
+      </h2>
+      <div class="links">
+        <a
+          :href="`mailto:${contacts.email}`"
+          :title="contacts.email">
+          <font-awesome-icon icon="envelope" />
+        </a>
+        <a
+          :href="contacts.linkedin"
+          title="LinkedIn">
+          <font-awesome-icon :icon="['fab', 'linkedin']" />
+        </a>
+        <a
+          :href="contacts.github"
+          title="Github">
+          <font-awesome-icon :icon="['fab', 'github']" />
+        </a>
+        <a
+          :href="contacts.gitlab"
+          title="Gitlab">
+          <font-awesome-icon :icon="['fab', 'gitlab']" />
+        </a>
+        <a
+          :href="cv"
+          title="Full resumeé (CV)">
+          <font-awesome-icon icon="file-alt" />
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: {
+        email: 'eu@henriquekuwai.com.br',
+        github: 'https://github.com/rikezenho',
+        gitlab: 'https://gitlab.com/rikezenho',
+        linkedin: 'http://br.linkedin.com/in/henriquekuwai'
+      },
+      cv: 'cv',
+      employee: 'Infracommerce',
+      employeeUrl: 'https://www.infracommerce.com.br'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  text-align: left;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1087px) {
+    max-width: 960px;
+    width: 960px;
+  }
+
+  @media screen and (max-width: 1087px) {
+    padding: 0 5%;
+  }
+
+  @media screen and (max-width: 480px) {
+    padding: 0 8%;
+  }
+}
+
+.title {
+  display: block;
+  font-weight: 700;
+  font-size: 80px;
+  letter-spacing: 1px;
+
+  @media screen and (max-width: 780px) {
+    font-size: 70px;
+
+    .text {
+      display: block;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 50px;
+  }
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+
+  @media screen and (max-width: 780px) {
+    font-size: 36px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 30px;
+  }
+}
+
+.highlight {
+  text-decoration: underline;
+  text-decoration-color: #a6e22e;
+}
+
+.links {
+  font-size: 40px;
+}
+</style>
